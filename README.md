a93c3c77ccc524a82c6d1c965b23d9715a81d1b3
Christian <christian.casaran@avantica.com>
2021-10-07 08:00:11 -0500

    Added CORS

---
+++ README.md
@@ -0,0 +1,9 @@
+# fastapi_mongodb_crud
+
+Python, FastAPI, MongoDb, Pydantic (BaseModel), Pymongo
+
+## **Start api:** uvicorn index:app --reload
+
+## **Front available in:**
+
+*https://github.com/Kriz1618/students-crud*
--- index.py
+++ index.py
@@ -1,10 +1,19 @@
 #  start with uvicorn index:app --reload
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors  import CORSMiddleware
+
+client_apps = ['http://localhost:3000'] #The react server
 
 #create app
 app = FastAPI()
-
+#Register apps wth CORS  middleware to allow resources sharing between different domains/origins
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = client_apps,
+  allow_credentials = True,
+  allow_methods = ['*'],
+  allow_headers = ['*']
+)
 #register routes
-
 app.include_router(student_router)
